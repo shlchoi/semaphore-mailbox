d64e0bbb6e28a02226434a70e224dc5f1240551e
Samson Choi <sh6choi@uwaterloo.ca>
2017-03-13 21:42:44 +0000

    Moving camera and luminosity sensor to state machine

--- states.py
+++ states.py
@@ -1,15 +1,23 @@
 from gpiozero import LED, Button
+from picamera import PiCamera
+from tsl2561 import TSL2561
 from requests import post
 from time import sleep
 from enum import Enum
+from os.path import isfile
+from json import load
+from requests import post
 
 
+camera = PiCamera()
 led_5 = LED(5)
 led_6 = LED(6)
 led_13 = LED(13)
-camera = LED(26)
+luminosity_sensor = TSL2561(debug=1)
 luminosity_button = Button(12)
 calibrate_button = Button(16)
+url = ''
+mailbox_id = ''
 
 
 class State(Enum):
@@ -21,27 +29,62 @@ class State(Enum):
     CALIBRATE_CLOSED = 5
 
 
-def take_picture():
+def load_config(config_path):
+    if isfile(config_path):
+        config = load(open(config_path, 'r'))
+        server_url = config['server_url']
+        server_port = config['server_port']
+	global url
+        url = '{0}:{1}'.format(server_url, server_port)
+	global mailbox_id
+        mailbox_id = config['mailbox_id']
+
+
+def take_picture(filename):
     led_5.on()
     led_6.on()
     led_13.on()
-    camera.on()
-    sleep(5)
-    camera.off()
+    sleep(3)
+    camera.capture(filename)
     led_5.off()
     led_6.off()
     led_13.off()
 
 
 def read_luminosity():
-    return luminosity_button.is_pressed
+    return int(luminosity_sensor.lux()) > 5
 
 
 def read_calibrate():
     return calibrate_button.is_pressed
 
 
+def send_snapshot():
+    endpoint = 'http://{}/snapshot'.format(url)
+    headers = {'enctype': 'multipart/form-data'}
+    files = {'snapshot': open('snapshot.jpg')}
+    data = {'mailbox': mailbox_id}
+
+    # r = post(endpoint, headers=headers, data=data, files=files)
+
+
+def send_calibration():
+    endpoint = 'http://{}/calibrate'.format(url)
+    headers = {'enctype': 'multipart/form-data'}
+    files = {'snapshot': open('calibrate.jpg')}
+    data = {'mailbox': mailbox_id}
+
+    # r = post(endpoint, headers=headers, data=data, files=files)
+
+
 def main():
+    load_config('config')
+    camera.brightness = 60
+    camera.resolution = (640, 480)
+    camera.color_effects = (128, 128)
+
+    print(url)
+    
     state = State.IDLE
     while True:
         calibrate = read_calibrate()
@@ -64,7 +107,8 @@ def main():
                 state = State.CLOSED
         elif state == State.CLOSED:
             # mailbox has been closed, do stuff here
-            take_picture()
+            take_picture('snapshot.jpg')
+            send_snapshot()
             print("State: IDLE")
             state = State.IDLE
         elif state == State.CALIBRATE_IDLE:
@@ -76,7 +120,8 @@ def main():
                print("State: CALIBRATE_CLOSED")
                state = State.CALIBRATE_CLOSED
         elif state == State.CALIBRATE_CLOSED:
-            take_picture()
+            take_picture('calibrate.jpg')
+            send_calibration()
             print("State: IDLE")
             state = State.IDLE
 
